a3a1eb724a30382efeb4a595b1e56a13b4321d61
NeloDraken <nelodraken@gmail.com>
2023-04-13 19:04:04 +0000

    saving image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,6 +20,9 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
+  applyFilter(reddify);
+  applyFilterNoBackground(increaseGreenByBlue);
+  applyFilterNoBackground(decreaseBlue);
 
   
 
@@ -32,18 +35,50 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
-
+function applyFilter(filterfunction){
+  for (i = 0; i <= image.length - 1; i++){
+    for(j = 0; j <= image[i].length - 1; j++){
+      rgbString = image[i][j];
+      rgbNumbers = rgbStringToArray(rgbString);
+      filterfunction(rgbNumbers);
+      rgbString = rgbArrayToString(rgbNumbers);
+      image[i][j] = rgbString;
+    }
+  }
+}
 
 // TODO 7: Create the applyFilterNoBackground function
-
+function applyFilterNoBackground(filterfunction){
+  background = image[0][0];
+  for (i = 0; i <= image.length - 1; i++){
+    for(j = 0; j <= image[i].length - 1; j++){
+      if (image[i][j] != background){
+        rgbString = image[i][j];
+        rgbNumbers = rgbStringToArray(rgbString);
+        filterfunction(rgbNumbers);
+        rgbString = rgbArrayToString(rgbNumbers);
+        image[i][j] = rgbString;
+      }
+    }
+  }
+}
 
 // TODO 5: Create the keepInBounds function
-
+function keepInBounds(num){
+  return Math.max(0, Math.min(num, 255))
+}
 
 // TODO 3: Create reddify function
-
+function reddify(rgb){
+  rgb[RED] = 200;
+}
 
 // TODO 6: Create more filter functions
-
+function decreaseBlue(rgb){
+  rgb[BLUE] = keepInBounds(rgb[BLUE] - 50);
+}
+function increaseGreenByBlue(rgb){
+  rgb[GREEN] = keepInBounds(rgb[GREEN] + rgb[BLUE]);
+}
 
 // CHALLENGE code goes below here
